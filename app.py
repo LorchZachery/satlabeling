f6c60064336fa21c46e54cd1fafe65b3499cd46f
LorchZachery <c21zachery.lorch@usafa.edu>
2020-07-22 11:20:47 -0600

    made filenames more global

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from image_render import image_render
 
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-fname = "static/uploads/test.tif"
+fname = "test"
 
 app = Flask(__name__)
 app.secret_key = "secret key"
@@ -40,12 +40,21 @@ def display_band():
             flash('No valied band selected')
             return redirect(request.url)
         else:
-            # run render_image class
-            image = image_render(fname,False)
-            rend_image = image.band(band)
+            # naming the file and checking to make sure it has not already 
+            # been created
+            filename = fname + '_' + str_band + '.jpg'
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+       
+            if not os.path.exists(filename): 
+                # run render_image class
+                # run render_image class
+                name = fname + '.tif'
+                name = os.path.join(app.config['UPLOAD_FOLDER'], name)
+                image = image_render(name,False)
+                rend_image = image.band(band)
             
-            #turn array into image
-            complete_image = Image.fromarray(rend_image)
+                #turn array into image
+                complete_image = Image.fromarray(rend_image)
             
             
     # if rgb is requested       
@@ -53,24 +62,32 @@ def display_band():
         # giving the file a name for later
         str_band = '321'
         band = 321
+        filename = fname + '_' + str_band + '.jpg'
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         
-        # run render_image class
-        image = image_render(fname,False)
-        rend_image = image.s2_to_rgb()
         
-        # turn array into image
-        complete_image = Image.fromarray(rend_image)
-   
+        # if the file has not been created, create it otherwise skip
+        if not os.path.exists(filename): 
+            # run render_image class
+            name = fname + '.tif'
+            name = os.path.join(app.config['UPLOAD_FOLDER'], name)
+            image = image_render(name,False)
+            rend_image = image.s2_to_rgb()
+        
+            # turn array into image
+            complete_image = Image.fromarray(rend_image)
+    
+    # if the file had not already been created save it
     # saving file as band number (321 for rgb) in the UPLOAD_FOLDER
-    filename = os.path.join(app.config['UPLOAD_FOLDER'], str_band)
-    complete_image.save(filename + '.jpg')
+    if not os.path.exists(filename): 
+        complete_image.save(filename)
        
     return render_template('image.html', band=band)
         
 @app.route('/image/<band>')
 def display_image(band):
 	# getting where the image is saved to display it    
-    return redirect(url_for('static', filename='uploads/' + band + '.jpg'), code=301)
+    return redirect(url_for('static', filename='uploads/' + fname + '_' + band + '.jpg'), code=301)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
