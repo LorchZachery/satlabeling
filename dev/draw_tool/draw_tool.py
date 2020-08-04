b71911006e0a787c8f7d813b0f36195dc6b0b4fa
Alexis <ajshirley2047@gmail.com>
2020-08-03 20:40:47 -0600

    draw update

--- dev/draw_tool/draw_tool.py
+++ dev/draw_tool/draw_tool.py
@@ -17,22 +17,22 @@ def line_drawing(event,x,y,flags,param):
 
     elif event==cv2.EVENT_MOUSEMOVE:
         if drawing==True:
-            cv2.line(img,(pt1_x,pt1_y),(x,y),color=(255,255,255),thickness=15)
+            cv2.line(image,(pt1_x,pt1_y),(x,y),color=(255, 255, 255),thickness=15)
             pt1_x,pt1_y=x,y
     elif event==cv2.EVENT_LBUTTONUP:
         drawing=False
         
-img = np.zeros((512,512,3), np.uint8)
+#img = np.zeros((512,512,3), np.uint8)
+image = cv2.imread("image.jpeg")
 cv2.namedWindow('img')
 cv2.setMouseCallback('img',line_drawing)
-
 
 def floodfill(x, y):
 
     #convert image to rbg
-    image = Image.fromarray(img)
-    image = image.convert("RGB")
-    pixdata = image.load()
+    #image = Image.fromarray(img)
+    #image = image.convert("RGB")
+    #pixdata = image.load()
     
     if pixdata[x,y] == (255, 255, 255): # the base case
         #print ("not white")
@@ -43,34 +43,32 @@ def floodfill(x, y):
         floodfill(x - 1, y) # left
         floodfill(x, y + 1) # down
         floodfill(x, y - 1) # up
-               
+        
+def mask():
+
+    Wmask =(image[:, :, 0:3] == [255, 255, 255]).all(2)
+    cv2.imwrite('result.png', (Wmask*255).astype(np.uint8))
+
+    #print(Wmask)
+             
 while(1):
-    
-    #for x in range(0, 512):
-        #for y in range(0, 512):
-            #cv2.line(img,(x,y+3),(x,y+100),color=(50,255,255),thickness= 1)
-            #pixdata[y-1, x] = (20, 255, 255)
 
     #show image on screen as drawing/filling
-    cv2.imshow('img',img)
+    cv2.imshow('img',image)
+    mask()
     
-    floodfill(0, 0)
+    #image = cv2.imread("result.png")
+    #cv2.imshow('masked image', image)
+    
+    #floodfill(0, 0)
 
-            
     if cv2.waitKey(1) & 0xFF == 27:
-        print("goodbye")
         break
         
-        
-
 
 cv2.destroyAllWindows()
 
-
-    
-
-
-            
-                
-
-        
+#for x in range(0, 512):
+    #for y in range(0, 512):
+        #cv2.line(img,(x,y+3),(x,y+100),color=(50,255,255),thickness= 1)
+        #pixdata[y-1, x] = (20, 255, 255)
