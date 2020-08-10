4ec42c5cad5b15a0920975983386d8ed1d350e11
alexisshirley <54089648+alexisshirley@users.noreply.github.com>
2020-08-09 19:25:11 -0600

    erase functionality

--- dev/draw_tool/static/script.js
+++ dev/draw_tool/static/script.js
@@ -6,6 +6,52 @@ var canvas, ctx, flag = false,
         currY = 0,
         dot_flag = false;
 
+<<<<<<< HEAD
+	var x = "opaque",
+        y = 5;
+
+    function init() {
+        canvas = document.getElementById('can');
+        ctx = canvas.getContext("2d");
+        
+        
+        //loads image to make canvas same size as image to be marked
+        imageObj = new Image();
+        imageObj.src= "/static/image.jpeg";
+        canvas.width = imageObj.width;
+        canvas.height = imageObj.height;
+        
+    
+        //tracks mouse movement
+        canvas.addEventListener("mousemove", function (e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            findxy('out', e)
+        }, false);
+    }
+      
+
+   
+    //draws on canvas
+    function draw() {
+        ctx.strokeStyle = 'rgba(255, 255, 255, .5)';
+        ctx.lineWidth = 5;
+        
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(currX, currY);
+        ctx.stroke();
+        ctx.closePath();
+        
+    }
+=======
 var x = "opaque",
        y = 5;
 var fill_value = true;
@@ -94,6 +140,7 @@ function draw() {
         ctx.fill();
         fill_value = false;
     }
+>>>>>>> 88fea1f37c61058414da88734f5c259e7cc467cf
     
     
     //ctx.closePath();
@@ -134,6 +181,46 @@ function findxy(res, e) {
             currY = e.clientY - canvas.offsetTop;
             draw();
         }
+<<<<<<< HEAD
+        if (res == 'move') {
+            if (flag) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.offsetLeft;
+                currY = e.clientY - canvas.offsetTop;
+                draw();
+            }
+        }
+        
+    }
+
+    function erasePen() {
+        alert("time to erase");
+        ctx.lineWidth = 50;
+        ctx.globalCompositeOperation = "destination-out";
+        ctx.strokeStyle = "rgba(0, 0, 0, 1.0)";
+    }
+    
+    function pen() {
+        alert("back to pen");
+        
+        ctx.lineWidth = 5;
+        ctx.globalCompositeOperation = "source-over";
+        ctx.strokeStyle = 'rgba(255, 255, 255, .5)';
+    }
+
+    function mask(){
+        alert("mask being made");
+       	var dataURL = canvas.toDataURL( "image/png" );
+		//alert(dataURL);
+      $.post( "/get_post_json", {
+        canvas_data: dataURL
+	  }, function(err, reg, resp) {
+		  
+	  });
+		
+	}
+=======
     }
     
 };
@@ -171,6 +258,7 @@ function mask(){
 	});
 	
 }
+>>>>>>> 88fea1f37c61058414da88734f5c259e7cc467cf
 	
 	
 	
